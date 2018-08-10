525099c2335e6310c3174c71fdcd6bb3e6c799d7
Dmytro Kazanzhy <dkazanzhy@gmail.com>
2018-08-10 19:33:00 +0300

    Update forms.py

--- info/forms.py
+++ info/forms.py
@@ -3,10 +3,10 @@ from registry.models import Region, District, Locality, Disease
 
 
 class ClinicsSearchForm(forms.Form):
-    region = forms.ModelChoiceField(queryset = Region.objects.all(), required=False)
-    district = forms.ModelChoiceField(queryset = District.objects.all(), required=False)
-    locality = forms.ModelChoiceField(queryset = Locality.objects.all(), required=False)
+    region = forms.ModelChoiceField(queryset = Region.objects.all(), required=False, label='Область')
+    district = forms.ModelChoiceField(queryset = District.objects.all(), required=False, label='Район')
+    locality = forms.ModelChoiceField(queryset = Locality.objects.all(), required=False, label='Населений пункт')
     
     
 class VaccinesSearchForm(forms.Form):
-    disease = forms.ModelChoiceField(queryset = Disease.objects.all(), required=False)
+    disease = forms.ModelChoiceField(queryset = Disease.objects.all(), required=False, label='Захворювання')
